6a2597bb9803d398cbea9622ad283c61ef205114
Josh Powell <josh@innovativeops.co>
2017-12-12 12:19:52 -0500

    Add first component

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -7,6 +7,5 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-import "./application.css";
-
-document.body.insertAdjacentHTML("afterbegin", "Webpacker works for Josh!");
+import "init";
+import "components/page/page";
